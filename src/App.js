54802e8ef1b0200a4afaf99583235b06a47318db
Arif Hossan <arif.hossan.eu@gmail.com>
2023-06-03 21:55:36 +0600

    rearrange router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, RouterProvider, Routes, createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Topics from "./components/Topics/Topics";
 import { topicsLoader } from "./DataLoader/topicsLoader";
+import Main from "./layouts/Main";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      children : [
+        {
+          path:'/',
+          element:<Home></Home>
+        },
+        {
+          path:'/home',
+          element:<Home></Home>
+        }
+      ]
+    },
+    {
+      path:'*',
+      element :<div>Error :404 </div>
+    }
+
+  ])
   return (
 
       <div className="App">
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/home" element={<Home></Home>}></Route>
-          <Route path="topics" loader={topicsLoader} element={<Topics></Topics>}></Route>
-        </Routes>
+       <RouterProvider router={router}></RouterProvider>
       </div>
 
   );
